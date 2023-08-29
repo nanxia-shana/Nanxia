fa935d1becf43faf250696fa6d90fcc27bfa824c
lijie <lijie@lijie.com>
2023-08-29 09:15:37 +0800

    修改音乐播放bug

--- src/components/threeJs/Sphere.vue
+++ src/components/threeJs/Sphere.vue
@@ -16,17 +16,18 @@ export default {
       const container = this.$refs.container;
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+      camera.position.z = 5;
+      // camera.position.set(-1000, 0, 0);
+      // camera.lookAt(-2000, 0, 0);
       const renderer = new THREE.WebGLRenderer();
 
       renderer.setSize(container.clientWidth, container.clientHeight);
       container.appendChild(renderer.domElement);
-
       const geometry = new THREE.SphereGeometry(1, 32, 32);
       const material = new THREE.MeshBasicMaterial({ map: texture });
       const sphere = new THREE.Mesh(geometry, material);
+      sphere.position.set(0, 0, 0);
       scene.add(sphere);
-
-      camera.position.z = 5;
 
       // 创建OrbitControls
       const controls = new OrbitControls(camera, renderer.domElement);
@@ -34,8 +35,8 @@ export default {
 
       function animate() {
         requestAnimationFrame(animate);
-        sphere.rotation.x += 0.001;
-        sphere.rotation.y += 0.01;
+        // sphere.rotation.x += 0.001;
+        // sphere.rotation.y += 0.01;
         renderer.render(scene, camera);
       }
 
