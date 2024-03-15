958d6a5352c722248fe1c2bd213ac91b6dc43a35
lijie <lijie@lijie.com>
2024-03-15 17:16:53 +0800

    add cropper test

--- src/views/person/Person.vue
+++ src/views/person/Person.vue
@@ -32,19 +32,27 @@
       </div>
     </div>
     <a-button type="primary" class="buttonP" @click="toCharacter">Character Card</a-button>
+    <div style="width: 800px; height: 30px; background-color:bisque;">{{!!cropperData.option.img}}----------{{ cropperData.option.img }}</div>
+    
+    <a-button type="primary" @click="rotateLeft">左旋转</a-button>
+    <a-button type="primary" @click="rotateRight">右旋转</a-button>
+    <a-button type="primary" @click="getCropDataBase64">确认截图</a-button>
     <a-modal v-model:visible="visible" title="Avatar" width="800px" :confirm-loading="confirmLoading" @ok="handleOk">
       <div class="modalBox">
         <div class="modalBox-left">
-          <img src="@/assets/images/Iraina3.jpg" alt="avater" />
+          <img :src="avaterCutUrl" alt="avater" />
         </div>
         <div class="modalBox-right">
           <a-upload-dragger
             v-model:fileList="fileList"
             name="avatar"
             accept=".jpg,.png,.jpeg"
+            :maxCount="1"
+            :show-upload-list="false"
             action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            @change="handleChange"
-            @drop="handleDrop">
+            :beforeUpload="handldBeforeUpload"
+            @drop="handleDrop"
+            @change="handleChange">
             <p class="ant-upload-drag-icon">
               <inbox-outlined></inbox-outlined>
             </p>
@@ -55,11 +63,27 @@
           </a-upload-dragger>
         </div>
       </div>
+      <div style="width: 300px; height: 300px; background-color:aqua;" v-if="!!cropperData.option.img">
+        <div>啊啊啊啊啊啊</div>
+        <vueCropper
+          style="width: 90%; height: 90%"
+          ref="cropper"
+          :img="cropperData.option.img"
+          :outputSize="cropperData.option.outputSize"
+          :outputType="cropperData.option.outputType"
+          :autoCrop="cropperData.option.autoCrop"
+          :autoCropWidth="cropperData.option.autoCropWidth"
+          :autoCropHeight="cropperData.option.autoCropHeight"
+        ></vueCropper>
+      </div>
     </a-modal>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref, getCurrentInstance } from "vue";
+// // 导入VueCropper
+import 'vue-cropper/dist/index.css'
+import { VueCropper } from "vue-cropper";
+import { ref, reactive, toRefs, getCurrentInstance } from "vue";
 import type { UploadChangeParam } from "ant-design-vue";
 import { EditOutlined, InboxOutlined, CheckCircleOutlined } from "@ant-design/icons-vue";
 import { useI18n } from "vue-i18n";
@@ -73,6 +97,18 @@ const confirmLoading = ref<boolean>(false);
 const fileList = ref([]);
 const nickname = ref<string>("Iraina");
 const isEditNickname = ref<boolean>(false);
+let cropperData = reactive({
+  option: {
+    img: "", //裁剪图片的地址
+    outputSize: 1, //outputSize 0~1
+    outputType: 'jpg', //裁剪生成图片的格式
+    autoCrop: true, //是否默认生成截图框
+    autoCropWidth: 150,//默认生成截图框宽度
+    autoCropHeight: 150,//默认生成截图框高度
+  },
+})
+const cropper: any = ref({})
+let avaterCutUrl = ref<any>(new URL('@/assets/images/Iraina3.jpg', import.meta.url).href)
 const avaterModify = () => {
   visible.value = true;
 };
@@ -83,14 +119,40 @@ const handleOk = () => {
     confirmLoading.value = false;
   }, 2000);
 };
-const handleDrop = (e: DragEvent) => {
-  console.log(e);
-};
 const avaterFocus = () => {
   avaterIsModify.value = true;
 };
 const avatarBlur = () => {
   avaterIsModify.value = false;
+};
+const handldBeforeUpload = (file: any) => {
+  // 判断文件格式
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    instance.proxy.$message.error('You can only upload JPG/PNG file!');
+    return false
+  }
+  // 判断文件大小
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    instance.proxy.$message.error('Image must smaller than 2MB!');
+    return false
+  }
+  // 获取文件的base64赋值给
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onload = function () {
+    const URL = window.URL || window.webkitURL
+    cropperData.option.img = URL.createObjectURL(file)
+    // cropperData.option.img = String(this.result)
+    avaterCutUrl = URL.createObjectURL(file)
+    console.log(cropperData.option.img,"aaaaaaaa");
+    return false
+  }
+  return false
+};
+const handleDrop = (e: DragEvent) => {
+  console.log("文件被拖入", e);
 };
 const handleChange = (info: UploadChangeParam) => {
   const status = info.file.status;
@@ -98,11 +160,26 @@ const handleChange = (info: UploadChangeParam) => {
     console.log(info.file, info.fileList);
   }
   if (status === "done") {
+    console.log("done");
     instance.proxy.$message.success(`${info.file.name} file uploaded successfully.`);
   } else if (status === "error") {
+    console.log("error");
     instance.proxy.$message.error(`${info.file.name} file upload failed.`);
   }
 };
+//向左旋转图片
+const rotateLeft = () => {
+  cropper.value.rotateLeft()
+}
+//向右旋转图片
+const rotateRight = () => {
+  cropper.value.rotateRight()
+}
+function getCropDataBase64() {
+  cropper.value.getCropData((data: any) => {
+    avaterCutUrl.value = data
+  })
+}
 const submitNickname = (nickname: any) => {
   isEditNickname.value = false;
   nickname.value = nickname;
